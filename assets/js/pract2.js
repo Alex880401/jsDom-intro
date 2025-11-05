a1dac8e211e6d14a009ced1024611ed75daf031f
Alex880401 <Derevyanko.Alexey88@gmail.com>
2025-11-05 22:45:08 +0200

    Add css

---
+++ assets/js/pract2.js
@@ -0,0 +1,18 @@
+const slides = [ 
+    {
+        src: 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?w=1000&h=1000&fit=crop',
+        alt: 'image1',
+    },
+    {
+        src: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1000&h=1000&fit=crop",
+        alt: 'image2',
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=1000&h=1000&fit=crop',
+        alt: 'image3',
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?w=1000&h=1000&fit=crop',
+        alt: 'image4',
+    },
+]
