6653a55d776755c405f5ff21c21f79d4f7d6bda7
Alex880401 <Derevyanko.Alexey88@gmail.com>
2025-11-25 19:04:15 +0200

    Finish bubbling

--- assets/js/bubbling.js
+++ assets/js/bubbling.js
@@ -4,3 +4,37 @@ const body = document.body;
 const section = document.querySelector('section');
 const button = document.querySelector('button');
 
+// function btnClickHandler(e) {
+//     //console.log(e);
+//     console.log(this); // = curent target
+//     // console.log(e.target); // = ціль події
+//     // console.log(e.currentTarget);
+    
+//     // Звернення до об'єкту, на якому відбувається подія
+//     //this.style.backgroundColor = 'yellow';
+
+//     console.log('--------------------------------------');
+// };
+
+// // занурення
+// body.addEventListener('click', btnClickHandler, {capture: true});
+// section.addEventListener('click', btnClickHandler, {capture: true});
+// button.addEventListener('click', btnClickHandler, {capture: true});
+
+// // спливання
+// button.addEventListener('click', btnClickHandler);
+// section.addEventListener('click', btnClickHandler);
+// body.addEventListener('click', btnClickHandler);
+
+// припинити розповсюдження події
+let clickCounter = 0;
+
+body.addEventListener('click', () => {
+    clickCounter++;
+    console.log(clickCounter);
+});
+
+button.addEventListener('click', e => {
+    e.stopPropagation(); // припиняє розповсюдження
+});
+
