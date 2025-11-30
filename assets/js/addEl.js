ac343b9132cc74762fce1383604ba03875dd1731
Alex880401 <Derevyanko.Alexey88@gmail.com>
2025-11-30 20:53:01 +0200

    Finish add elements

--- assets/js/addEl.js
+++ assets/js/addEl.js
@@ -1,1 +1,29 @@
 'use strict'
+
+const section = document.createElement('section');
+
+const divRoot = document.querySelector('#root');
+
+// додавання дочірнього елемента
+
+divRoot.append(section);
+section.textContent = 'Lorem ipsum';
+section.style.backgroundColor = 'grey';
+
+const article = document.createElement('article');
+divRoot.prepend(article);
+article.textContent = 'The first child'
+
+// додавання сіблінгів
+
+const btn = document.createElement('button');
+section.after(btn);
+btn.textContent = 'New button';
+
+// видалення 
+
+const divForRemove = document.querySelector('.removable');
+
+divForRemove.addEventListener('click', function() {
+    this.remove();
+});
